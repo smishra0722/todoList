a1cf4283cf49ef156c2927d7d2039c470c78eb7f
canteenms <m.shivam9988@gmail.com>
2020-11-28 14:58:37 +0530

    Fixed a bug related to background image

--- js/app.js
+++ js/app.js
@@ -7,11 +7,11 @@ function modeToggle() {
         body.classList.toggle('dark');
         body.classList.toggle('light');
         if(window.getComputedStyle( document.body ,null).getPropertyValue('background-color') === 'rgb(22, 23, 34)') {
-            topp.style.backgroundImage = "url('../images/bg-desktop-light.jpg')";
+            topp.style.backgroundImage = "url('images/bg-desktop-light.jpg')";
             toggleIcon.classList.remove('fa-moon');
             toggleIcon.classList.add('fa-sun');
         } else if(window.getComputedStyle( document.body ,null).getPropertyValue('background-color') === 'rgb(250, 250, 250)'){
-            topp.style.backgroundImage = "url('../images/bg-desktop-dark.jpg')";
+            topp.style.backgroundImage = "url('images/bg-desktop-dark.jpg')";
             toggleIcon.classList.remove('fa-sun');
             toggleIcon.classList.add('fa-moon');
         }
