22f977ded4dc640bccd1102149e5866cfcaa2b1c
canteenms <m.shivam9988@gmail.com>
2020-11-28 14:51:30 +0530

    Added toggle for dark and light mode

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+let topp = document.querySelector('.top');
+let toggleIcon = document.querySelector('.toggle-icon');
+function modeToggle() {
+    toggle.addEventListener('click', () => {
+        body.classList.toggle('dark');
+        body.classList.toggle('light');
+        if(window.getComputedStyle( document.body ,null).getPropertyValue('background-color') === 'rgb(22, 23, 34)') {
+            topp.style.backgroundImage = "url('../images/bg-desktop-light.jpg')";
+            toggleIcon.classList.remove('fa-moon');
+            toggleIcon.classList.add('fa-sun');
+        } else if(window.getComputedStyle( document.body ,null).getPropertyValue('background-color') === 'rgb(250, 250, 250)'){
+            topp.style.backgroundImage = "url('../images/bg-desktop-dark.jpg')";
+            toggleIcon.classList.remove('fa-sun');
+            toggleIcon.classList.add('fa-moon');
+        }
+    });
+}
+
+modeToggle();
